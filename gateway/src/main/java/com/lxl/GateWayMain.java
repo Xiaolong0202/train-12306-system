52b8ecc7879d5753dc0a76f538c2433b6b884e54
liuxiaolong <1343024614@qq.com>
2023-09-19 19:38:53 +0800

    新增网关模块

---
+++ gateway/src/main/java/com/lxl/GateWayMain.java
@@ -0,0 +1,18 @@
+package com.lxl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author LiuXiaolong
+ * @Description Default (Template) Project
+ * @DateTime 2023/9/19  19:17
+ **/
+
+
+@SpringBootApplication
+public class GateWayMain {
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayMain.class);
+    }
+}
