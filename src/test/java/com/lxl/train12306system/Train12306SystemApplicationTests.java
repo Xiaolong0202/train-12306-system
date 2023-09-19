584254bad8a7feaddf8d9c507f45598b59714aa3
liuxiaolong <1343024614@qq.com>
2023-09-19 12:37:00 +0800

    初始化项目

---
+++ src/test/java/com/lxl/train12306system/Train12306SystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lxl.train12306system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Train12306SystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
