585583757613cbf702e945828ee41c2ee244a126
liuxiaolong <1343024614@qq.com>
2023-09-19 16:58:25 +0800

    新增member模块

---
+++ member/src/main/java/com/lxl/member/MemberMain.java
@@ -0,0 +1,16 @@
+package com.lxl.member;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author LiuXiaolong
+ * @Description Default (Template) Project
+ * @DateTime 2023/9/19  16:53
+ **/
+@SpringBootApplication
+public class MemberMain {
+    public static void main(String[] args) {
+        SpringApplication.run(MemberMain.class);
+    }
+}
