0d055e55e458eecd1fb693b54241a2cc603952d3
liuxiaolong <1343024614@qq.com>
2023-09-20 17:05:33 +0800

    绘制了登录页面

--- web/src/main.js
+++ web/src/main.js
@@ -4,6 +4,12 @@ import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css';
+import * as Icons from '@ant-design/icons-vue';
 
+let app = createApp(App);
+app.use(store).use(router).use(Antd).mount('#app');
+//引入图标
+for (let iconsKey in Icons) {
+    app.component(iconsKey,Icons[iconsKey])
+}
 
-createApp(App).use(store).use(router).use(Antd).mount('#app')
---
+++ web/src/views/LoginView.vue
@@ -0,0 +1,71 @@
+<template>
+    <a-row>
+        <a-col :span="8" :offset="8" class="login-main">
+            <a-form
+                    :model="formState"
+                    name="basic"
+                    autocomplete="off"
+                    @finish="onFinish"
+                    @finishFailed="onFinishFailed"
+            >
+
+                <a-form-item>
+                    <h1 style="text-align: center"><ThunderboltTwoTone />&nbsp;售票系统</h1>
+                </a-form-item>
+                <a-form-item
+                        :rules="[{ required: true, message: '请输入手机号!' }]"
+                >
+                    <a-input v-model:value="formState.mobile" placeholder="请输入手机号" size="large"/>
+                </a-form-item>
+
+                <a-form-item
+                        :rules="[{ required: true, message: '请输入验证码!' }]"
+                >
+                    <a-input-search
+                            v-model:value="formState.code"
+                            placeholder="请输入验证码"
+                            enter-button="发送验证码"
+                            size="large"
+                            @search="onMessage"
+                    />
+                </a-form-item>
+
+                <a-form-item>
+                    <a-button type="primary" html-type="submit" size="large">登录</a-button>
+                </a-form-item>
+            </a-form>
+        </a-col>
+    </a-row>
+</template>
+
+<script setup>
+import {reactive} from 'vue';
+
+const formState = reactive({
+    mobile: '',
+    code: '',
+});
+const onFinish = values => {
+    console.log('Success:', values);
+};
+const onFinishFailed = errorInfo => {
+    console.log('Failed:', errorInfo);
+};
+
+const onMessage = () => {
+    console.log("发送短信")
+    console.log(formState.code);
+    console.log(formState.mobile);
+}
+</script>
+
+<style scoped>
+    .login-main{
+        margin-top: 15%;
+        padding: 30px;
+        border: 2px grey solid;
+        border-radius: 10px;
+        background-color: #f5fff5;
+        width: 80%;
+    }
+</style>
