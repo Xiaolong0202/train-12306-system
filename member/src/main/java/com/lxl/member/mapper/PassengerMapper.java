b4ed3c3864c9cb03f9aae369edc49073bcd4b5b5
liuxiaolong <1343024614@qq.com>
2023-09-22 15:18:39 +0800

    新建了passenger表,与乘客类型枚举

---
+++ member/src/main/java/com/lxl/member/mapper/PassengerMapper.java
@@ -0,0 +1,18 @@
+package com.lxl.member.mapper;
+
+import com.lxl.member.domain.Passenger;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 13430
+* @description 针对表【passenger(乘客)】的数据库操作Mapper
+* @createDate 2023-09-22 15:11:55
+* @Entity com.lxl.member.domain.Passenger
+*/
+public interface PassengerMapper extends BaseMapper<Passenger> {
+
+}
+
+
+
+
--- sql/member.sql
+++ sql/member.sql
@@ -5,4 +5,18 @@ drop table if exists `member`;
 create table `member`(
      `id` bigint not null primary key  comment 'ID' ,
      `mobile` varchar(11) unique not null comment '手机号'
-)
+);
+
+#乘客表 ,乘客与会员关联，会员为乘客购买车票
+drop table if exists `passenger`;
+
+create table `passenger` (
+    `id` bigint not null  primary key comment 'id',
+    `member_id` bigint not null comment '会员id',
+    `name` varchar(20) not null comment '乘车人姓名',
+    `id_card` varchar(18) not null comment '身份证',
+    `type` char(1) not null comment '旅客类型|枚举',
+    `create_time` datetime not null comment '创建时间',
+    `update_time` datetime not null comment '修改时间',
+    index(`member_id`)#创建索引便于查找
+)comment '乘客'
