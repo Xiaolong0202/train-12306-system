79fe50ffa5b576cc73d95a5587332ec046bfc8f8
liuxiaolong <1343024614@qq.com>
2023-09-24 17:03:42 +0800

    定义了全局变量

--- web/src/main.js
+++ web/src/main.js
@@ -6,9 +6,10 @@ import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css';
 import * as Icons from '@ant-design/icons-vue';
 import {configAxios} from "@/config/axiosConfig";
+import * as myenum from "../src/assets/myenum"
 
 let app = createApp(App);
-app.use(store).use(router).use(Antd).mount('#app');
+app.use(store).use(router).use(Antd).use(myenum).mount('#app');
 //引入图标
 for (let iconsKey in Icons) {
     app.component(iconsKey,Icons[iconsKey])
--- web/src/views/main/PassengerView.vue
+++ web/src/views/main/PassengerView.vue
@@ -100,7 +100,7 @@ const passenger = reactive({
 })
 
 
-const passenger_type = [{key:"1",value:"成人"},{key:"2",value:"学生"},{key:"3",value:"小孩"}]
+const passenger_type = window.PASSENGER_TYPE
 const open = ref(false);
 const loading = ref(false);
 const showModal = () => {
