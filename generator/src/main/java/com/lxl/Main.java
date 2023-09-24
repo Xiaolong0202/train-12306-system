b2ee33e0bbf22c0f699673ae4c5f9680a00a6214
liuxiaolong <1343024614@qq.com>
2023-09-25 00:10:24 +0800

    引入了freemarker,可以根据模板生成代码

---
+++ generator/src/main/java/com/lxl/Main.java
@@ -0,0 +1,12 @@
+package com.lxl;
+
+/**
+ * @Author LiuXiaolong
+ * @Description Default (Template) Project
+ * @DateTime 2023/9/24  23:46
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ generator/src/main/java/com/lxl/generator/ftl/test.ftl
@@ -0,0 +1,5 @@
+
+
+public class ${domain}{
+    private String name;
+}
