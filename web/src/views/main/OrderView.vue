f3a2d9ac882f863fe4b89c668425e954dfa54fa7
liuxiaolong <1343024614@qq.com>
2023-10-04 11:39:05 +0800

    完成了order界面的票数与架构的显示

--- web/src/views/main/OrderView.vue
+++ web/src/views/main/OrderView.vue
@@ -1,16 +1,33 @@
 <template>
     <div style="float: left;">
         <span class="ticketInfo">{{ ticketInfo.train.startDate }}</span>&nbsp;
-        <span class="ticketInfo">{{ticketInfo.train.type}}{{ticketInfo.train.code}}</span>次&nbsp;
-        <span class="ticketInfo">{{ticketInfo.start}}</span>站&nbsp;
-        <span class="ticketInfo">({{ticketInfo.startTime}}开)</span>站&nbsp;
+        <span class="ticketInfo">{{ ticketInfo.train.type }}{{ ticketInfo.train.code }}</span>次&nbsp;
+        <span class="ticketInfo">{{ ticketInfo.start }}</span>站&nbsp;
+        <span class="ticketInfo">({{ ticketInfo.startTime }}开)</span>站&nbsp;
         ——
-        <span class="ticketInfo">{{ticketInfo.end}}</span>&nbsp;
-        <span class="ticketInfo">({{ticketInfo.startTime}}到)</span>&nbsp;
+        <span class="ticketInfo">{{ ticketInfo.end }}</span>&nbsp;
+        <span class="ticketInfo">({{ ticketInfo.startTime }}到)</span>&nbsp;
         <hr>
+        <div style="float: left">
+            <span v-if="ticketInfo.ydz>=0">
+                一等座: <span class="ticketSell">{{ ticketInfo.ydzPrice }}￥  &nbsp;{{ ticketInfo.ydz }}</span>&nbsp;张票
+            </span>
+            <span v-if="ticketInfo.edz>=0">
+                二等座: <span class="ticketSell">{{ ticketInfo.edzPrice }}￥  &nbsp;{{ ticketInfo.edz }}</span>&nbsp;张票
+            </span>
+            <span v-if="ticketInfo.yw>=0">
+                硬卧: <span class="ticketSell">{{ ticketInfo.ywPrice }}￥  &nbsp;{{ ticketInfo.yw }}</span>&nbsp;张票
+            </span>
+            <span v-if="ticketInfo.rw>=0">
+                软卧: <span class="ticketSell">{{ ticketInfo.rwPrice }}￥ &nbsp;{{ ticketInfo.rw }}</span>&nbsp;张票
+        </span>
+        </div>
     </div>
+    <br>
+    <div>
 
-
+    </div>
+  <!--    {{ticketInfo}}-->
 
 
 </template>
@@ -22,8 +39,14 @@ const ticketInfo = JSON.parse(sessionStorage.getItem(SESSION_ORDER) || '{}')
 </script>
 
 <style scoped>
-    .ticketInfo{
-        font-size: 30px;
-        font-weight: bold;
-    }
+.ticketInfo {
+    font-size: 30px;
+    font-weight: bold;
+}
+
+.ticketSell {
+    font-size: 20px;
+    font-weight: bolder;
+    color: red;
+}
 </style>
